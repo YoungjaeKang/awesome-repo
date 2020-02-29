af90fcd2c0ebe0e277f41a02988550010b882a35
yjk <kyj8028@hotmail.com>
2020-03-01 00:03:44 +0900

    #1.1 Start Coding

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.core import serializers
+# from django.http import HttpResponse
+# from django.shortcuts import render
+# from rooms.models import Room
 
-# Create your views here.
+# def list_rooms(request):
+#     # import json을 해서 for문으로 queryset을 json에 넣으려고 해도 안된다.
+#     # 그래서 serializer를 사용해야 한다.
+#     data = serializers.serialize("json", Room.objects.all())
+#     response = HttpResponse(content=data)
+#     return response
