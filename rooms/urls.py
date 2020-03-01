0aa47d57e0ddc45275274d8e547fed81e03be030
yjk <kyj8028@hotmail.com>
2020-03-01 12:54:20 +0900

    #1.3 Introduction Complete

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,13 +1,22 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
 from . import views
+from . import viewsets
 
 app_name = "rooms"
 
+# viewsets.py
+router = DefaultRouter()
+router.register("", viewsets.RoomViewset, basename='room')
+
+urlpatterns = router.urls
+
+
 # CBV
-urlpatterns = [
-    path("list/", views.ListRoomsView.as_view()),
-    path("<int:pk>/", views.SeeRoomView.as_view()),
-]
+# urlpatterns = [
+#     path("list/", views.ListRoomsView.as_view()),
+#     path("<int:pk>/", views.SeeRoomView.as_view()),
+# ]
 
 
 
